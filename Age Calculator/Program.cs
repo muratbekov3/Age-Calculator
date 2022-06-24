1385fc60c190c2d39b198267fcd67c9abf6c21cf
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2022-06-24 19:15:53 +0200

    first commit

---
+++ Age Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿
+					
+public class Program
+{
+    public static void Main( string[] args)
+    {
+        DateTime bday = Convert.ToDateTime("01/01/2002");
+        DateTime today = DateTime.Today;
+        int age = today.Year - bday.Year;
+		
+        if(bday > today){
+            Console.WriteLine("Incorrect date");
+        }
+		
+        Console.WriteLine("Your age {0}", age);
+			
+    }
+}
